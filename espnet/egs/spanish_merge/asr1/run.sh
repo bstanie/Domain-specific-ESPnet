47f189b68bea2efb9316de1e5b547a31cc074c10
Stanislav Barabanov <stsbar@gmail.com>
2020-06-19 15:53:40 +0300

    WIP

--- espnet/egs/spanish_merge/asr1/run.sh
+++ espnet/egs/spanish_merge/asr1/run.sh
@@ -97,9 +97,9 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
     fbankdir=fbank
 
     # select datasets for train, dev, test. You can choose any dataset from "datasets" variable which was preprocessed earlier
-    utils/combine_data.sh  data/${train_set}_org data/train_crowdsource
-    utils/combine_data.sh  data/${train_dev}_org data/test_crowdsource
-    utils/combine_data.sh  data/${recog_set}_org data/train_gong
+    utils/combine_data.sh  data/${train_set}_org data/train_tedx
+    utils/combine_data.sh  data/${train_dev}_org data/test_tedx
+    utils/combine_data.sh  data/${recog_set}_org data/train_gong data/test_gong
 
     # Generate the fbank features; by default 80-dimensional fbanks with pitch on each frame
     for x in ${train_set} ${train_dev} ${recog_set}; do
