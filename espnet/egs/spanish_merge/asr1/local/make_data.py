2a76027f9d059240a5c03679b183ca58905e4837
Stanislav Barabanov <stsbar@gmail.com>
2020-06-21 11:47:12 +0300

    WIP

--- espnet/egs/spanish_merge/asr1/local/make_data.py
+++ espnet/egs/spanish_merge/asr1/local/make_data.py
@@ -47,7 +47,7 @@ raw_data_folder = Path(eg_dir, 'raw_data')
 
 
 def prepare_public_data_factory(args):
-    print(args)
+    logger.info(args)
     for dataset in args:
         logger.info(f"\n\nDownloading and extracting data for '{dataset.name}' dataset\n\n")
 
