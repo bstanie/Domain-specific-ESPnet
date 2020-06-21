49a34855a8cf5701dc77874e0740bbba201a52e7
Stanislav Barabanov <stsbar@gmail.com>
2020-06-21 12:26:47 +0300

    WIP

--- espnet/egs/spanish_merge/asr1/local/data_preparation.sh
+++ espnet/egs/spanish_merge/asr1/local/data_preparation.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
-pip install -r requirements.txt
+#pip install -r requirements.txt
 
 #add-apt-repository ppa:mc3man/trusty-media
 #apt-get -y update
 #apt install -y ffmpeg
 
+echo $@
 python3 make_data.py --datasets "$@"
 
 chmod -R 777 ../data
