bff5faf0cbb1cb24ec267f2b6c42f25cb56c7d47
Stanislav Barabanov <stsbar@gmail.com>
2020-07-08 15:21:59 +0300

    WIP

--- espnet/docker/train.sh
+++ espnet/docker/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 0,1,2,3,4,5,6,7,8,9,10,11 --docker_egs spanish_merge/asr1 \
---ngpu 12 --stage 4 --stop_stage 999
+./run.sh --docker_gpu 0,1,2,3,4,5,6,7 --docker_egs spanish_merge/asr1 \
+--ngpu 8 --stage 4 --stop_stage 999
