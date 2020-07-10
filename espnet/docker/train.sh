5c60e040615990316d548192123759b5c4ac3ac9
Stanislav Barabanov <stsbar@gmail.com>
2020-07-10 12:20:30 +0300

    add a new eg for gong data only

--- espnet/docker/train.sh
+++ espnet/docker/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-./run.sh --docker_gpu 0,1,2,3,4,5,6,7 --docker_egs spanish_merge/asr1 \
---ngpu 8 --stage 5 --stop_stage 999
+./run.sh --docker_gpu 0,1,2,3,4,5,6,7 --docker_egs spanish_gong/asr1 \
+--ngpu 8 --stage 0 --stop_stage 999
